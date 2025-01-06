226a0076d37cde5372f30eaf825ef0336200a1c2
Sava Aleksic <sa20210462@student.fon.bg.ac.rs>
2025-01-06 15:19:23 +0100

    Kreirana login forma

--- Domaci2/blog/src/App.js
+++ Domaci2/blog/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import LoginForm from './Components/LoginForm/LoginForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+        <LoginForm></LoginForm>
     </div>
   );
 }
