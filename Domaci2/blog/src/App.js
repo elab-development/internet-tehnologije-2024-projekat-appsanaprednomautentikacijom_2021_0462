6acdfcad318a2d34c8ae4265612d3351e4b4109e
Sava Aleksic <sa20210462@student.fon.bg.ac.rs>
2025-01-08 19:11:28 +0100

    Kreirana Header komponenta

--- Domaci2/blog/src/App.js
+++ Domaci2/blog/src/App.js
@@ -2,13 +2,17 @@
 import LoginForm from './Components/LoginForm/LoginForm.jsx';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import NavBar from './Components/NavBar/NavBar.jsx';
+import Header from './Components/Header/Header.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="" element={<LoginForm/>}/>
-        <Route path="home" element={<NavBar/>}/>
+        <Route path="home" element={<>
+          <NavBar/>
+          <Header/>
+          </>}/>
       </Routes>  
     </BrowserRouter>
   );
---
+++ Domaci2/blog/src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+
+import './Header.css';
+
+function Header(){
+
+    return(
+        <div className="container">
+        <div className="card">
+          <div className="card-content">
+            <h2 className="card-title">Explore with us</h2>
+            <button className="card-button">Click Me</button>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+
+export default Header;
+
