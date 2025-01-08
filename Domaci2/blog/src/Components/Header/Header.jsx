c38d752c7aab49cd9af4a59fbcadf1ec61e0c3a2
Sava Aleksic <sa20210462@student.fon.bg.ac.rs>
2025-01-08 19:16:27 +0100

    Ispravljanje gresaka

--- Domaci2/blog/src/Components/Header/Header.jsx
+++ Domaci2/blog/src/Components/Header/Header.jsx
@@ -8,7 +8,7 @@ function Header(){
         <div className="card">
           <div className="card-content">
             <h2 className="card-title">Explore with us</h2>
-            <button className="card-button">Click Me</button>
+            <button className="card-button">Explore!</button>
           </div>
         </div>
       </div>
