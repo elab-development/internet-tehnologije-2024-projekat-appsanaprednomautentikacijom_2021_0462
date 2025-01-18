3d6dc8eb2e8c034ad2e277fe8fe114f22693fdf7
Vukasin Popovic <ambitiouself4@gmail.com>
2025-01-18 14:57:13 +0100

    Restilizovanje card komponente

--- Domaci2/blog/src/Components/Card/Card.jsx
+++ Domaci2/blog/src/Components/Card/Card.jsx
@@ -5,10 +5,17 @@ const Card = ({ image, title, description, buttonText, personName }) => {
   return(
     <div class="page-container">
   <div class="centered-div">
-    <div class="title">My React Cards</div>
+    <div class="title">
+        <h1>TravelBlog</h1>
+        <p className='title-card'>Explore destinations and experiences from all over the world!</p>
+    </div>
     <div class="card-container">
       <div class="card">1</div>
       <div class="card">2</div>
+      <div class="card">3</div>
+      <div class="card">3</div>
+      <div class="card">3</div>
+      <div class="card">3</div>
       <div class="card">3</div>
       <div class="card">3</div>
       
