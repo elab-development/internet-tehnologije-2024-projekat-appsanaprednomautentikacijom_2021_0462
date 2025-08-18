277a30d3f49f95f79015da3133a1704648bb6c18
zico89 <ambitiouself4@gmail.com>
2025-08-18 13:24:44 +0200

    Napravi ruter za korisnika

---
+++ seminarski/src/routers/userRouter.mjs
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import UserController from "../controllers/UserController.mjs";
+const router = Router();
+const controller = new UserController();
+
+router.post("/register", controller.register.bind(controller));
+
+router.post("/confirm", controller.confirm.bind(controller));
+
+router.post("/login", controller.login.bind(controller));
+
+router.post("/confirm2fa", controller.confirm2fa.bind(controller));
+
+router.post("/confirmLogin", controller.confirmLogin.bind(controller));
+
+export default router;
+
